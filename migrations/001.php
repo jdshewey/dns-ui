9372f2282aee8c93c2e4f2d942a72abd17fd716a
James Shewey <jdshewey@gmail.com>
2020-01-13 11:55:55 -0700

    Migration errors because table already exists
    Also, MariaDB doesn't use OIDs and timestamps cannot be with or without timezone. By default MariaDB converts from the local timezone to UTC at storage time and back during the select.

--- migrations/001.php
+++ migrations/001.php
@@ -2,10 +2,10 @@
 $migration_name = "Add migration support";
 
 $this->database->exec("
-CREATE TABLE migration (
+CREATE TABLE IF NOT EXISTS migration (
     id integer NOT NULL,
     name text NOT NULL,
-    applied timestamp without time zone NOT NULL,
+    applied timestamp NOT NULL,
     CONSTRAINT migration_pkey PRIMARY KEY (id)
-) WITH (OIDS=FALSE)
+)
 ");
