8eccf99fac9d788261611c853556308af6adaf3f
James Shewey <jdshewey@gmail.com>
2020-01-13 14:26:30 -0700

    More updates for MariaDB compatibility

--- migrations/002.php
+++ migrations/002.php
@@ -8,125 +8,111 @@ try {
 	$this->database->exec('SELECT * FROM config');
 } catch(PDOException $e) {
 
-	$this->database->exec('
-	CREATE TABLE change (
-		id NOT NULL AUTO_INCREMENT,
-		changeset_id integer NOT NULL,
-		before bytea,
-		after bytea,
-		CONSTRAINT change_pkey PRIMARY KEY (id)
-	)
-	');
+        $this->database->exec('
+        CREATE TABLE `change` (
+                id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                changeset_id integer NOT NULL,
+                `before` blob,
+                after blob
+        )
+        ');
 
-	$this->database->exec('
-	CREATE TABLE changeset (
-		id serial,
-		zone_id integer NOT NULL,
-		author_id integer NOT NULL,
-		change_date timestamp without time zone NOT NULL,
-		comment text,
-		deleted integer DEFAULT 0 NOT NULL,
-		added integer DEFAULT 0 NOT NULL,
-		requester_id integer,
-		CONSTRAINT changeset_pkey PRIMARY KEY (id)
-	)
-	');
+        $this->database->exec('
+        CREATE TABLE changeset (
+                id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                zone_id integer NOT NULL,
+                author_id integer NOT NULL,
+                change_date timestamp NOT NULL,
+                comment text,
+                deleted integer DEFAULT 0 NOT NULL,
+                added integer DEFAULT 0 NOT NULL,
+                requester_id integer
+        )
+        ');
 
-	$this->database->exec('
-	CREATE TABLE config (
-		id integer NOT NULL,
-		default_soa_template integer,
-		default_ns_template integer,
-		CONSTRAINT config_pkey PRIMARY KEY (id)
-	)
-	');
-	$this->database->exec('INSERT INTO "config" (id) VALUES (1)');
+        $this->database->exec('
+        CREATE TABLE config (
+                id integer NOT NULL PRIMARY KEY,
+                default_soa_template integer,
+                default_ns_template integer
+        )
+        ');
+        $this->database->exec('INSERT INTO `config` (id) VALUES (1)');
 
-	$this->database->exec('
-	CREATE TABLE ns_template (
-		id serial,
-		name text NOT NULL,
-		nameservers text NOT NULL,
-		CONSTRAINT ns_template_pkey PRIMARY KEY (id),
-		CONSTRAINT ns_template_name_key UNIQUE (name)
-	)
-	');
+        $this->database->exec('
+        CREATE TABLE ns_template (
+                id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                name varchar(256) NOT NULL UNIQUE,
+                nameservers text NOT NULL
+        )
+        ');
 
-	$this->database->exec('
-	CREATE TABLE pending_update (
-		id serial,
-		zone_id integer NOT NULL,
-		author_id integer,
-		request_date timestamp without time zone NOT NULL,
-		raw_data bytea NOT NULL,
-		CONSTRAINT pending_change_pkey PRIMARY KEY (id)
-	)
-	');
+        $this->database->exec('
+        CREATE TABLE pending_update (
+                id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                zone_id integer NOT NULL,
+                author_id integer,
+                request_date timestamp NOT NULL,
+                raw_data blob NOT NULL
+        )
+        ');
 
-	$this->database->exec('
-	CREATE TABLE soa_template (
-		id serial,
-		name text NOT NULL,
-		primary_ns text NOT NULL,
-		contact text NOT NULL,
-		refresh integer NOT NULL,
-		retry integer NOT NULL,
-		expire integer NOT NULL,
-		default_ttl integer NOT NULL,
-		soa_ttl integer,
-		CONSTRAINT soa_template_pkey PRIMARY KEY (id),
-		CONSTRAINT soa_template_name_key UNIQUE (name)
-	)
-	');
+	        $this->database->exec('
+        CREATE TABLE soa_template (
+                id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                name varchar(256) NOT NULL UNIQUE,
+                primary_ns text NOT NULL,
+                contact text NOT NULL,
+                refresh integer NOT NULL,
+                retry integer NOT NULL,
+                expire integer NOT NULL,
+                default_ttl integer NOT NULL,
+                soa_ttl integer
+        )
+        ');
 
-	$this->database->exec('
-	CREATE TABLE "user" (
-		id serial,
-		uid text,
-		name text,
-		email text,
-		auth_realm ENUM ("local","LDAP"),,
-		active integer,
-		admin integer DEFAULT 0 NOT NULL,
-		developer integer DEFAULT 0 NOT NULL,
-		csrf_token text,
-		CONSTRAINT user_pkey PRIMARY KEY (id),
-		CONSTRAINT user_uid_key UNIQUE (uid)
-	)
-	');
+        $this->database->exec('
+        CREATE TABLE user (
+                id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                uid varchar(256) UNIQUE,
+                name text,
+                email text,
+                auth_realm ENUM ("local","LDAP"),
+                active integer,
+                admin integer DEFAULT 0 NOT NULL,
+                developer integer DEFAULT 0 NOT NULL,
+                csrf_token text
+        )
+        ');
 
-	$this->database->exec('
-	CREATE TABLE user_alert (
-		id serial,
-		user_id integer,
-		class text,
-		content text,
-		escaping integer,
-		CONSTRAINT user_alert_pkey PRIMARY KEY (id)
-	)
-	');
+        $this->database->exec('
+        CREATE TABLE user_alert (
+                id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                user_id integer,
+                class text,
+                content text,
+                escaping integer
+        )
+        ');
 
-	$this->database->exec('
-	CREATE TABLE zone (
-		id serial,
-		pdns_id text,
-		name text,
-		serial bigint,
-		active boolean DEFAULT true NOT NULL,
-		account text,
-		CONSTRAINT zone_pkey PRIMARY KEY (id),
-		CONSTRAINT zone_pdns_id_key UNIQUE (pdns_id)
-	)
-	');
+        $this->database->exec('
+        CREATE TABLE zone (
+                id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                pdns_id varchar(256) UNIQUE,
+                name text,
+                serial bigint,
+                active boolean DEFAULT true NOT NULL,
+                account text
+        )
+        ');
 
-	$this->database->exec("
-	CREATE TABLE zone_access (
-		zone_id integer NOT NULL,
-		user_id integer NOT NULL,
-		level ENUM ('administrator', 'operator') NOT NULL,
-		CONSTRAINT zone_admin_pkey PRIMARY KEY (zone_id, user_id)
-	)
-	");
+        $this->database->exec("
+        CREATE TABLE zone_access (
+                zone_id integer NOT NULL PRIMARY KEY,
+                user_id integer NOT NULL,
+                level ENUM ('administrator', 'operator') NOT NULL
+        )
+        ");
 
 	$this->database->exec('
 	ALTER TABLE change
